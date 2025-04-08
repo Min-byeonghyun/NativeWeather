0fb44a4c6e838ea47f85b5a9e3a91ef0956a4c2a
Min-byeonghyun <qudgus5125@naver.com>
2025-04-08 23:39:53 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>안녕하세요!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 28,
+    fontWeight: "bold",
+  },
+});
